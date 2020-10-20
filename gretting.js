11c7578ce72864543dff02c63b0badfd20e046d8
jiwon03 <bjiwon03@gmail.com>
2020-10-20 11:59:30 +0900

    Saving the user name part2

--- gretting.js
+++ gretting.js
@@ -2,9 +2,22 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     gretting = document.querySelector(".js-gretting");
 
-    const USER_LS = "currentUser",
+const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGretting(currentValue);
+    saveName(currentValue);
+}
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 function paintGretting(text) {
     form.classList.remove(SHOWING_CN);
     gretting.classList.add(SHOWING_CN);
@@ -13,7 +26,7 @@ function paintGretting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null) {
-        //he is not
+        askForName();
     } else {
         paintGretting(currentUser);
     }
