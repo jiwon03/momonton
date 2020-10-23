7b9d7bd9e2792bb72b4b1451469067b566d543d9
jiwon03 <bjiwon03@gmail.com>
2020-10-23 23:21:52 +0900

    Getting the weather part2

--- weather.js
+++ weather.js
@@ -1,20 +1,34 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "4c6a21c0d3a62d1473f8c7de70e109f6";
 const COORDS = "coords";
 
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json()
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 function handleGeoSucces(position) {
-    const latitude = position.coords.latitude;
-    const longtitude = position.coords.longtitude;
+    const latitude = position.coords.latitude,
+         longitude = position.coords.longitude;
     const coordsObj = {
         latitude,
-        longtitude
-    }
+        longitude
+    };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 function handleGeoError() {
-    console.log("cant");
+    console.log("can't get position");
 }
 function askForCoords(){
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
@@ -24,7 +38,8 @@ function loadCoords() {
     if(loadedCoords === null) {
         askForCoords();
     } else {
-        //getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 function init() {
